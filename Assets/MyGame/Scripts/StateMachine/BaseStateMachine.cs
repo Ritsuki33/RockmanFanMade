481d58a6effa67056d694c019a66b4f8d424cc8a
Ritsuki33 <intellpro0918@gmail.com>
2024-08-24 17:18:19 +0900

    BaseStateMachine

--- Assets/MyGame/Scripts/StateMachine/BaseStateMachine.cs
+++ Assets/MyGame/Scripts/StateMachine/BaseStateMachine.cs
@@ -2,14 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public interface IBaseState<T>
+
+public interface IBaseState<T> where T : MonoBehaviour
 {
+    bool Immediate { get; }
+
     void Enter(T obj, int preId);
+    IEnumerator EnterCoroutine(T obj, int preId);
 
     void FixedUpdate(T obj);
     void Update(T obj);
 
     void Exit(T obj, int nextId);
+    IEnumerator ExitCoroutine(T obj, int nextId);
 }
 
 
@@ -17,8 +22,14 @@ public interface IBaseState<T>
 /// 状態ノード
 /// </summary>
 /// <typeparam name="T"></typeparam>
-public class BaseState<T> : IBaseState<T>
+public class BaseState<T> : IBaseState<T> where T : MonoBehaviour
 {
+    public bool immediate { get; private set; }
+    public BaseState(bool immediate = true)
+    {
+        this.immediate = immediate;
+    }
+
     virtual protected void Enter(T obj, int preId) { }
     virtual protected IEnumerator EnterCoroutine(T obj, int preId) { yield return null; }
 
@@ -28,19 +39,23 @@ public class BaseState<T> : IBaseState<T>
     virtual protected void Exit(T obj, int nextId) { }
     virtual protected IEnumerator ExitCoroutine(T obj, int nextId) { yield return null; }
 
+    bool IBaseState<T>.Immediate => immediate;
     void IBaseState<T>.Enter(T obj, int preId) { Enter(obj, preId); }
+    IEnumerator IBaseState<T>.EnterCoroutine(T obj, int preId) { yield return EnterCoroutine(obj, preId); }
 
     void IBaseState<T>.FixedUpdate(T obj) { FixedUpdate(obj); }
     void IBaseState<T>.Update(T obj) { Update(obj); }
 
     void IBaseState<T>.Exit(T obj, int nextId) { Exit(obj, nextId); }
+    IEnumerator IBaseState<T>.ExitCoroutine(T obj, int nextId) { yield return ExitCoroutine(obj, nextId); }
+
 }
 
 /// <summary>
 /// ステートマシン
 /// </summary>
 /// <typeparam name="T"></typeparam>
-public class BaseStateMachine<T>  where T : BaseStateMachine<T>
+public class BaseStateMachine<T> : MonoBehaviour where T : BaseStateMachine<T>
 {
     Dictionary<int, IBaseState<T>> states = new Dictionary<int, IBaseState<T>>();
 
@@ -106,12 +121,36 @@ public class BaseStateMachine<T>  where T : BaseStateMachine<T>
 
             curId = requestId;
             requestId = -1;
-            // 出口処理
-            curState?.Exit(obj, curId);
-            curState = states[curId];
-            nextState = null;
-            // 入口処理
-            curState?.Enter(obj, preId);
+            if (true || nextState.Immediate)
+            {
+                // 出口処理
+                curState?.Exit(obj, curId);
+                curState = states[curId];
+                nextState = null;
+                // 入口処理
+                curState?.Enter(obj, preId);
+            }
+            else
+            {
+                if (coroutine != null)
+                {
+                    obj.StopCoroutine(coroutine);
+                }
+                coroutine = obj.StartCoroutine(TransitStateCoroutine(obj, curId));
+            }
         }
+    }
+
+    IEnumerator TransitStateCoroutine(T obj, int requestId)
+    {
+        // 出口処理
+        if (curState != null) yield return curState.ExitCoroutine(obj, curId);
+
+        curState = states[requestId];
+
+        // 入口処理
+        yield return curState.EnterCoroutine(obj, preId);
+
+        coroutine = null;
     }
 }
