81a6128c8cfd970198ea3b5ae768976909de0580
Ritsuki33 <intellpro0918@gmail.com>
2024-09-11 16:43:04 +0900

    ベジェ曲線運動

---
+++ Assets/BezierCurveMovement.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using UnityEngine;
+
+public static class BezierCurveMovement
+{
+
+    public static Vector2 GetVelocity(Vector2 curPos, float timeElapsed, float duration, Transform a, Transform b, Transform c, Action callback = null)
+    {
+
+        // 移動時間を計算
+        float t = timeElapsed / duration;
+
+        // tが1を超えたらコールバック呼び出し(コールバックがない場合は無視)
+        if (callback != null && t > 1f)
+        {
+            t = 1f;
+            callback?.Invoke();
+        }
+
+        // 2次ベジェ曲線の計算
+        Vector2 position = Mathf.Pow(1 - t, 2) * a.position
+                         + 2 * (1 - t) * t * b.position
+                         + Mathf.Pow(t, 2) * c.position;
+
+        Vector2 movement = position - curPos;
+
+        return movement / Time.fixedDeltaTime;
+    }
+}
