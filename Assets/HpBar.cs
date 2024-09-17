67f38859c4a1467d1f6f82ba57a495d8c4cead62
Ritsuki33 <intellpro0918@gmail.com>
2024-09-17 17:51:16 +0900

    HpBar　正規化

--- Assets/HpBar.cs
+++ Assets/HpBar.cs
@@ -8,8 +8,7 @@ public class HpBar : MonoBehaviour
     [SerializeField] Transform root;
     List<GameObject> list = new List<GameObject>();
 
-    int maxHp => list.Count;
-    int currentHp = 0;
+    float currentHp = 0;
 
     Coroutine coroutine = null;
 
@@ -26,14 +25,18 @@ public class HpBar : MonoBehaviour
         currentHp = 0;
     }
 
-    public void SetParam(int val)
+    /// <summary>
+    /// パラメータの更新
+    /// </summary>
+    /// <param name="val">0～1の範囲</param>
+    public void SetParam(float val)
     {
 
-        int start = Mathf.Min(currentHp, val);  // 開始位置は小さい方
-        int end = Mathf.Max(currentHp, val);    // 終了位置は大きい方
+        int start = (int)(Mathf.Min(currentHp, val) * list.Count);  // 開始位置は小さい方
+        int end = (int)(Mathf.Max(currentHp, val) * list.Count);    // 終了位置は大きい方
         bool isIncreasing = val > currentHp;    // 増減フラグ
 
-        for (int i = start; i < end && i < maxHp; i++)
+        for (int i = start; i < end && i < list.Count; i++)
         {
             list[i].SetActive(isIncreasing);
         }
@@ -41,18 +44,23 @@ public class HpBar : MonoBehaviour
         currentHp = val;
     }
 
-    public void ParamChangeAnimation(int val, Action fisnihCallback = null)
+    /// <summary>
+    /// パラメータの更新アニメーション
+    /// </summary>
+    /// <param name="val"></param>
+    /// <param name="fisnihCallback"></param>
+    public void ParamChangeAnimation(float val, Action fisnihCallback = null)
     {
         if (coroutine != null) { StopCoroutine(coroutine); }
         coroutine = StartCoroutine(CoParamChangeAnimation(val));
 
-        IEnumerator CoParamChangeAnimation(int val)
+        IEnumerator CoParamChangeAnimation(float val)
         {
-            int start = Mathf.Min(currentHp, val);  // 開始位置は小さい方
-            int end = Mathf.Max(currentHp, val);    // 終了位置は大きい方
+            int start = (int)(Mathf.Min(currentHp, val) * list.Count);  // 開始位置は小さい方
+            int end = (int)(Mathf.Max(currentHp, val) * list.Count);    // 終了位置は大きい方
             bool isIncreasing = val > currentHp;    // 増減フラグ
 
-            for (int i = start; i < end && i < maxHp; i++)
+            for (int i = start; i < end && i < list.Count; i++)
             {
                 list[i].SetActive(isIncreasing);
                 yield return new WaitForSeconds(0.1f);
