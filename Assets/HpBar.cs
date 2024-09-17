d796571595ef40caef11c3abbf63ca89b5acddff
Ritsuki33 <intellpro0918@gmail.com>
2024-09-17 17:34:27 +0900

    Hpバー

---
+++ Assets/HpBar.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HpBar : MonoBehaviour
+{
+    [SerializeField] Transform root;
+    List<GameObject> list = new List<GameObject>();
+
+    int maxHp => list.Count;
+    int currentHp = 0;
+
+    Coroutine coroutine = null;
+
+    private void Awake()
+    {
+        // 子オブジェクトをすべて取得
+        for (int i = 0; i < root.childCount; i++)
+        {
+            Transform child = root.GetChild(i);
+            child.gameObject.SetActive(false);
+            list.Add(child.gameObject);
+        }
+
+        currentHp = 0;
+    }
+
+    public void SetParam(int val)
+    {
+
+        int start = Mathf.Min(currentHp, val);  // 開始位置は小さい方
+        int end = Mathf.Max(currentHp, val);    // 終了位置は大きい方
+        bool isIncreasing = val > currentHp;    // 増減フラグ
+
+        for (int i = start; i < end && i < maxHp; i++)
+        {
+            list[i].SetActive(isIncreasing);
+        }
+
+        currentHp = val;
+    }
+
+    public void ParamChangeAnimation(int val, Action fisnihCallback = null)
+    {
+        if (coroutine != null) { StopCoroutine(coroutine); }
+        coroutine = StartCoroutine(CoParamChangeAnimation(val));
+
+        IEnumerator CoParamChangeAnimation(int val)
+        {
+            int start = Mathf.Min(currentHp, val);  // 開始位置は小さい方
+            int end = Mathf.Max(currentHp, val);    // 終了位置は大きい方
+            bool isIncreasing = val > currentHp;    // 増減フラグ
+
+            for (int i = start; i < end && i < maxHp; i++)
+            {
+                list[i].SetActive(isIncreasing);
+                yield return new WaitForSeconds(0.1f);
+            }
+
+            currentHp = val;
+            fisnihCallback?.Invoke();
+            coroutine = null;
+        }
+    }
+}
--- Assets/MyGame/Scripts/Manager/UiManager.cs
+++ Assets/MyGame/Scripts/Manager/UiManager.cs
@@ -8,6 +8,10 @@ public class UiManager : SingletonComponent<UiManager>
     [SerializeField] ReadyUi readyUi = default;
     [SerializeField] FadeInManager fadeInManager = default;
 
+    [SerializeField] HpBar hpBar = default;
+
     public ReadyUi ReadyUi => readyUi;
     public FadeInManager FadeInManager => fadeInManager;
+
+    public HpBar HpBar => hpBar;
 }
