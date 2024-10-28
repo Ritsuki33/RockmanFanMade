4c63e0b0cb955a1dda2da6491cfd8a3841e4f7c1
Ritsuki33 <intellpro0918@gmail.com>
2024-10-28 12:26:14 +0900

    テキストタイピング

---
+++ Assets/MyGame/Scripts/Ui/LetterRevealText.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections;
+using TMPro;
+using UnityEngine;
+
+public class LetterRevealText : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI text;
+    IEnumerator enumerator = null;
+
+    bool IsPlaying => enumerator != null;
+    bool isPause = false;
+    /// <summary>
+    /// プレイ
+    /// </summary>
+    /// <param name="delay"></param>
+    /// <param name="callback"></param>
+    public void Play(float delay, Action callback = null)
+    {
+        enumerator = LetterRevealCo(delay);
+        StartCoroutine(enumerator);
+        isPause = false;
+    }
+
+    /// <summary>
+    /// 再開
+    /// </summary>
+    public void Resume()
+    {
+        if(enumerator!=null) StartCoroutine(enumerator);
+        isPause = false;
+    }
+
+    /// <summary>
+    /// ポーズ
+    /// </summary>
+    public void Pause()
+    {
+        if (enumerator != null) StopCoroutine(enumerator);
+    }
+
+    /// <summary>
+    /// 停止
+    /// </summary>
+    public void Stop()
+    {
+        if (enumerator != null)
+        {
+            StopCoroutine(enumerator);
+            enumerator = null;
+        }
+    }
+
+    /// <summary>
+    /// テキストタイピング
+    /// </summary>
+    /// <param name="delay"></param>
+    /// <param name="callback"></param>
+    /// <returns></returns>
+    IEnumerator LetterRevealCo(float delay, Action callback = null)
+    {
+        for (int i = 0; i <= text.text.Length; i++)
+        {
+            text.maxVisibleCharacters = i;
+
+            yield return new WaitForSeconds(delay);
+        }
+
+        callback?.Invoke();
+
+        enumerator = null;
+    }
+}
