e0cd5d3baf5110a0442649fe1abeb5e2b7125410
Ritsuki33 <intellpro0918@gmail.com>
2025-02-02 21:34:50 +0900

    ReactiveProperty

---
+++ Assets/MyGame/Scripts/Manager/Tool/ReactiveProperty.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+public interface IReadOnlyReactiveProperty<T>
+{
+    void Subscribe(Action<T> onChangePropertyCallback);
+}
+
+public class ReactiveProperty<T> : IReadOnlyReactiveProperty<T>
+{
+    T value;
+
+    public ReactiveProperty(T value)
+    {
+        this.value = value;
+    }
+
+    public T Value
+    {
+        get { return this.value; }
+        set
+        {
+            this.value = value;
+            _onChangePropertyCallback?.Invoke(value);
+        }
+    }
+
+    public void Subscribe(Action<T> onChangePropertyCallback)
+    {
+        _onChangePropertyCallback = onChangePropertyCallback;
+    }
+
+    event Action<T> _onChangePropertyCallback = default;
+}
