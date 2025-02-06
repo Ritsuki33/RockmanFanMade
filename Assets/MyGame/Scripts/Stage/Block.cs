5cd322777e279df66c6caa983cb05cbf47ab4fce
Ritsuki33 <intellpro0918@gmail.com>
2025-02-06 12:06:15 +0900

    Blockの破壊

--- Assets/MyGame/Scripts/Stage/Block.cs
+++ Assets/MyGame/Scripts/Stage/Block.cs
@@ -6,17 +6,27 @@ public class Block : MonoBehaviour
 {
 
     //private ExplodePool ExplodePool => EffectManager.Instance.ExplodePool;
+    RbCollide rbCollide=new RbCollide();
 
+    private void Awake()
+    {
+        rbCollide.Init();
+
+        rbCollide.onTriggerEnterRockBusterDamage += OnTriggerEnterRockBusterBase;
+    }
+
+    private void OnTriggerEnterRockBusterBase(RockBusterDamage damage)
+    {
+        Debug.Log("破壊！");
+        this.gameObject.SetActive(false);
+        var effect = ObjectManager.Instance.OnGet<PsObject>(PoolType.BlockBreakEffect);
+        effect.transform.position = this.transform.position;
+
+        damage.projectile.Delete();
+    }
+    
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        //if (collision.gameObject.CompareTag("RockBuster"))
-        //{
-        //    Destroy(this.gameObject);
-        //    var explode = ExplodePool.Pool.Get();
-
-        //    explode.transform.position = this.transform.position;
-        //    var rockBuster = collision.gameObject.GetComponent<Projectile>();
-        //    rockBuster?.Delete();
-        //}
+        rbCollide.OnTriggerEnter(collision);
     }
 }
