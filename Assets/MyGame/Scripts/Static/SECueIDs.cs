7697ad3b4937fed4aebe4adec4dc95a45f70c645
Ritsuki33 <intellpro0918@gmail.com>
2025-02-11 00:33:27 +0900

    サウンド素材＋static変数スクリプト自動生成ダイアログ

---
+++ Assets/MyGame/Scripts/Static/SECueIDs.cs
@@ -0,0 +1,18 @@
+﻿public static class SECueIDs
+{
+    public static readonly int charge = 0;
+    public static readonly int athit = 2;
+    public static readonly int buster = 3;
+    public static readonly int cn1 = 4;
+    public static readonly int block = 5;
+    public static readonly int chargeshot = 6;
+    public static readonly int menu = 7;
+    public static readonly int select = 8;
+    public static readonly int explosion = 9;
+    public static readonly int tyakuti = 10;
+    public static readonly int shatter = 11;
+    public static readonly int teleportin = 12;
+    public static readonly int teleportout = 13;
+    public static readonly int hprecover = 14;
+    public static readonly int minishot = 15;
+}
