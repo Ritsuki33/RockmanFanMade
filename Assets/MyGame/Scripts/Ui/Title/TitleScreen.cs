8223f43215b0133d179314017467c7d6a1eacb06
Ritsuki33 <intellpro0918@gmail.com>
2025-02-28 16:08:02 +0900

    タイトル　演出

--- Assets/MyGame/Scripts/Ui/Title/TitleScreen.cs
+++ Assets/MyGame/Scripts/Ui/Title/TitleScreen.cs
@@ -1,9 +1,11 @@
-﻿using UnityEngine;
+﻿using DG.Tweening;
+using UnityEngine;
+using UnityEngine.UI;
 
 public class TitleScreen : BaseScreen<TitleScreen, TitleScreenPresenter, TitleManager.ScreenType>
 {
     [SerializeField] MainMenuColorSelect select;
-
+    [SerializeField] private Image flashImage;
     public MainMenuColorSelect Select => select;
 
     protected override void Open()
@@ -14,5 +16,15 @@ public class TitleScreen : BaseScreen<TitleScreen, TitleScreenPresenter, TitleMa
     protected override void Hide()
     {
         FadeInManager.Instance.FadeOutImmediate();
+    }
+
+    public void Flash()
+    {
+        flashImage.color = new Color(1, 1, 1, 0); // 初期状態を透明に
+        flashImage.DOFade(1, 0.5f) // フェードイン
+            .OnComplete(() => flashImage.DOFade(0, 0.5f).Play())
+            .Play(); // フェードアウト
+
+        AudioManager.Instance.PlaySe(SECueIDs.shakin);
     }
 }
