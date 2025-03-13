760fc91c560d5afdac9901b3eaa14e50b317c0fd
Ritsuki33 <intellpro0918@gmail.com>
2025-03-13 21:17:00 +0900

    説明文

--- Assets/MyGame/Scripts/Spawn/ObjectSpawn.cs
+++ Assets/MyGame/Scripts/Spawn/ObjectSpawn.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class ObjectSpawn : Spawn<BaseObject>
 {
+    [Header("AddressablesロードでIDを指定してオブジェクトの生成")]
     [SerializeField] string path;
     [SerializeField] int id;
     protected override BaseObject OnGetResource()
